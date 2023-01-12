d2f3be5a48c8fa978aa3b20b11ee757cc6f593be
loretovega <loreto.vega@carsales.com>
2023-01-12 17:17:26 -0300

    Initial commit

---
+++ app/src/main/java/com/example/composecourseyt/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.example.composecourseyt
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composecourseyt.ui.theme.ComposeCourseYTTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            
+            Row {
+
+/*
+                ComposeCourseYTTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(
+                        modifier = Modifier.fillMaxSize(),
+                        color = MaterialTheme.colors.background
+                    ) {
+                        Greeting("Lolocita")
+                    }*/
+                    Text("Wena ")
+                    Text(text = "choro ")
+                    Text(text = "Greg")
+
+                }
+
+            }
+        }
+    }
+
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeCourseYTTheme {
+        Greeting("Android")
+    }
+}
